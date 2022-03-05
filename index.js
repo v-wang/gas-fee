7246459dc2afdc42b2a00233166108a648de79e9
v-wang <victorwang920@gmail.com>
2022-03-05 14:11:47 -0500

    set for controller to handle data fetch

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+// dependencies
 require('dotenv').config();
 const express = require('express');
 const app = express();
 
+// configs
+app.use(express.json());
+app.use('/data', dataController.router);
+
 //https://ethgasstation.info/api/ethgasAPI.json?api-key=
+
+const appController = require('./src/controllers/appController');
+const dataController = require('./src/controllers/dataController');
 
 app.listen(4000, () => {
   console.log('app listening on port 4000');
 });
+
+module.exports = app;
--- src/controllers/dataController.js
+++ src/controllers/dataController.js
@@ -2,3 +2,13 @@
 
 // Fetch data from ETH Gas Station API
 // https://docs.ethgasstation.info/
+
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+router.get('/', (req, res) => {
+  res.json({ status: 'ok' });
+});
+
+module.exports = router;
