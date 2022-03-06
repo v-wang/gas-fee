9de3b1174fb957c2c33c44a07a988f1f18ed9b20
v-wang <victorwang920@gmail.com>
2022-03-06 18:08:21 -0500

    TargetFee schema completed

--- src/controllers/dataController.js
+++ src/controllers/dataController.js
@@ -7,18 +7,28 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-setInterval(() => {
-  axios
-    .get(
-      `https://ethgasstation.info/api/ethgasAPI.json?api-key=${process.env.ETH_GAS_STATION_KEY}`
-    )
-    .then((data) => {
-      console.log(data.data);
-    })
-    .then((data) => {
-      // res.send(data);
-    });
-}, 4000);
+const url = 'https://ethgasstation.info/api/ethgasAPI.json?api-key=';
+
+const fetchFee = () => {
+  setInterval(() => {
+    axios
+      .get(`${url}${process.env.ETH_GAS_STATION_KEY}`)
+      .then((data) => {
+        console.log(data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, 4000);
+};
+
+const saveFee = async (fee) => {};
+
+const calcHourAvg = async (fees) => {};
+
+const saveHourAvg = async (avg) => {};
+
+// fetchFee();
 
 router.get('/ping', (req, res) => {
   res.json({ status: 'ok' });
